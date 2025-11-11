d520d6001727c113943fc567f6d1e29772778ffa
encryptedcryptowallet <encryptedcryptowallet@gmail.com>
2025-11-11 06:13:43 +0000

    feat: initial commit

--- backend/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  appwrite: {
-    endpoint: 'https://fra.cloud.appwrite.io/v1',
-    projectId: 'zapway',
-    apiKey: 'standard_db0af1217dde553df3f2a42d5e5300a39855e297d91e7e5c53f0aa4064bd03be8d3e2063ec073a391aa41e0f41910c38834a5e02c151bc79e6974decd8876526491b158e872b362cb71009a409d9c506cc5aa924fd5742aca0df0ce9ba5898ece2c584529151d35503f531bf62da9ce8e983939757479ed40852c358406bd579',
-  },
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,6 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
-  },
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    }
-  },
-  build: {
-    outDir: 'dist'
-  }
 });
